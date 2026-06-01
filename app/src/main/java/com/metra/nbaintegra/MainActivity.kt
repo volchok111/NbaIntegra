510ff39104907e486cffa2d060d183b14bec34e8
Artem Volkov <artem.volkov@astrumq.com>
2026-06-01 22:26:12 +0200

    feat: navigation setup
    - setup nav3
    - add routes
    - add NbaNavHost.kt
    - edit PlayersScreen.kt

--- app/src/main/java/com/metra/nbaintegra/MainActivity.kt
+++ app/src/main/java/com/metra/nbaintegra/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.metra.nbaintegra.core.theme.NbaIntegraTheme
-import com.metra.nbaintegra.feature.players.PlayerScreen
+import com.metra.nbaintegra.navigation.NbaNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NbaIntegraTheme {
-                PlayerScreen()
+                NbaNavHost()
             }
         }
     }
