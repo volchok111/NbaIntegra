4c7423277214ad917ff3d16a89187fb0efb0bd39
Artem Volkov <artem.volkov@astrumq.com>
2026-06-02 14:57:17 +0200

    feat: splash screen
    - add splash screen
    - add animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
+    implementation(libs.android.lottie.compose)
+
     implementation(libs.timber)
 
     testImplementation(libs.junit)
